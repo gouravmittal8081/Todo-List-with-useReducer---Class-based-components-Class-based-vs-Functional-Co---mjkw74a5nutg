55f8735b048f1ef48087af47eea05cdc47d7cb7f
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-10-14 11:44:39 +0530

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/App.css";
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="logo">Todo List</div>
+      <button className="theme">Theme</button>
+    </div>
+  );
+};
+
+export default Navbar;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
