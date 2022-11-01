81426b17ec644c2bc15593ece2e6e329ee468e85
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-11-02 00:00:06 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,28 +1,16 @@
 const todoReducer = (state, action) => {
   switch (action.type) {
-    case "ADD": {
-      return {
-        todos: [
-          ...state.todos,
-          {
-            title: action.value,
-            id:
-              new Date().toLocaleDateString() +
-              "" +
-              new Date().toLocaleTimeString(),
-          },
-        ],
-      };
-    }
-    case "DELETE": {
-      return {
-        todos: action.data,
-      };
-    }
-    default: {
+    case "add_todo":
+      return [...state, newTodo(action.payload.title)];
+    case "delete_todo":
+      return state.filter((task) => task.id !== action.payload.id);
+    default:
       return state;
-    }
   }
 };
+
+function newTodo(task) {
+  return { id: Date.now(), title: task };
+}
 
 export { todoReducer };
