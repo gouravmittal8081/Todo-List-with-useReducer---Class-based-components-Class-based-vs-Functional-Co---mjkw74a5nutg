75a27cdc616012226a16744a645c8d43dfd6823c
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-11-01 23:58:48 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { todoReducer } from "../reducers/todo";
 import "../styles/App.css";
-const Todo = ({ title, id, handleDelete }) => {
+import { AddTodo } from "./AddTodo";
+import { Todo } from "./Todo";
+const App = () => {
+  const [state, dispatch] = useReducer(todoReducer, []);
+
   return (
-    <div className="todo">
-      <div className="todo-title">{title}</div>
-      <button className="todo-delete" onClick={() => handleDelete(id)}>
-        Delete
-      </button>
+    <div id="main">
+      <AddTodo dispatch={dispatch} />
+      {state.map((task) => (
+ 
+        <Todo
+          key={task.id}
+          id={task.id}
+          value={task.title}
+          dispatch={dispatch}
+        />
+
+      ))}
     </div>
   );
 };
 
-export { Todo };
+export default App;
